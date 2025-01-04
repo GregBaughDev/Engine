150138fdf26a14c10ceb78bee3b6ae2fae1fe256
GregBaughDev <gregbaughdesign@outlook.com>
2025-01-04 19:52:53 +1100

    Add Log class

--- Engine.h
+++ Engine.h
@@ -2,6 +2,7 @@
 
 // For use by client applications
 #include "./src/Application.h"
+#include "./src/log/Log.h"
 
 // Entry point
 #include "./src/EntryPoint.h"
--- src/EntryPoint.h
+++ src/EntryPoint.h
@@ -1,10 +1,15 @@
 #pragma once
 #include "./Application.h"
+#include "./log/Log.h"
 
 extern Engine::Application* Engine::CreateApplication();
 
 int main(int argc, char** argv)
-{
+{    
+    Engine::Log::Init();
+    ENGINE_CORE_WARN("Initialised Log!");
+    ENGINE_INFO("Hello!");
+
     auto app = Engine::CreateApplication();
     app->Run();
     delete app;
